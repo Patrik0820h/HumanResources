9517a87ae750205b676fd06b3e12a404efd4f954
Podiashvili Luka 956 <podluk956@logiker.hu>
2026-04-24 09:40:03 +0200

    Database fix

--- HRA/HRA/Database.cs
+++ HRA/HRA/Database.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
@@ -17,29 +18,6 @@ namespace HRA
                 conn.Open();
 
 
-                // People table - Insert
-                string createPeopleTable = "CREATE TABLE IF NOT EXISTS People (Id INT AUTO_INCREMENT PRIMARY KEY, FirstName VARCHAR(255), LastName VARCHAR(255))";
-                using (MySqlCommand cmd = new MySqlCommand(createPeopleTable, conn))
-                {
-                    cmd.ExecuteNonQuery();
-                    Console.WriteLine("People Tábla sikeresen létrehozva!");
-                }
-
-                string insertPeopleTable = "INSERT INTO People (FirstName, LastName) VALUES (@FirstName, @LastName)";
-                using (MySqlCommand cmd = new MySqlCommand(insertPeopleTable, conn))
-                {
-                    for (int i = 0; i <= adatok.Count - 1; i++) 
-                    {
-                        cmd.Parameters.Clear();
-                        cmd.Parameters.AddWithValue("@FirstName", adatok[i].FirstName);
-                        cmd.Parameters.AddWithValue("LastName", adatok[i].LastName);
-                        cmd.ExecuteNonQuery();
-
-                    }
-                    Console.WriteLine("People tábla sikeresen feltöltve");
-                }
-
-
                 // Department table - Insert
                 string createDepartmentTable = "CREATE TABLE IF NOT EXISTS Department (Id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(255))";
                 using (MySqlCommand cmd = new MySqlCommand(createDepartmentTable, conn))
@@ -51,10 +29,11 @@ namespace HRA
                 string insertDepartmentTable = "INSERT INTO Department (Name) VALUES (@Name)";
                 using (MySqlCommand cmd = new MySqlCommand(insertDepartmentTable, conn))
                 {
-                    for(int i = 0;i <= adatok.Count - 1; i++)
+                    var DepartmentID = adatok.Select(dep => dep.Department).Distinct();
+                    foreach (var item in DepartmentID)
                     {
                         cmd.Parameters.Clear();
-                        cmd.Parameters.AddWithValue("@Name", adatok[i].Department);
+                        cmd.Parameters.AddWithValue("@Name", item);
                         cmd.ExecuteNonQuery();
                     }
                     Console.WriteLine("Department tábla sikeresen feltöltve");
@@ -72,10 +51,11 @@ namespace HRA
                 string insertJobPositionTable = "INSERT INTO JobPosition (JobName) VALUES (@JobName)";
                 using (MySqlCommand cmd = new MySqlCommand(insertJobPositionTable, conn))
                 {
-                    for (int i = 0; i <= adatok.Count - 1; i++)
+                    var jobposdis = adatok.Select(jobpos => jobpos.JobTitle).Distinct().OrderBy(jobpos => jobpos);
+                    foreach (var item in jobposdis)
                     {
                         cmd.Parameters.Clear();
-                        cmd.Parameters.AddWithValue("@JobName", adatok[i].JobTitle);
+                        cmd.Parameters.AddWithValue("@JobName", item);
                         cmd.ExecuteNonQuery();
                     }
                     Console.WriteLine("JobPosition tábla sikeresen feltöltve");
@@ -83,52 +63,76 @@ namespace HRA
 
 
                 // Employee Table - Insert
-                string createEmployeeTable = "CREATE TABLE IF NOT EXISTS Employee (Id INT AUTO_INCREMENT PRIMARY KEY, GrossWage VARCHAR(255), NetWage VARCHAR(255), People_ID INT, JobPosition_ID INT, Department_ID INT, CONSTRAINT FK_Employee_PeopleID FOREIGN KEY (People_ID) REFERENCES People (Id), CONSTRAINT FK_Employee_DepartmentID FOREIGN KEY (Department_ID) REFERENCES Department (Id), CONSTRAINT FK_Employee_JobPositionID FOREIGN KEY (JobPosition_ID) REFERENCES JobPosition (Id))";
+                string createEmployeeTable = "CREATE TABLE IF NOT EXISTS Employee (Id INT AUTO_INCREMENT PRIMARY KEY, FirstName VARCHAR(255), LastName VARCHAR(255), GrossWage VARCHAR(255), NetWage VARCHAR(255), JobPosition_ID INT, Department_ID INT, CONSTRAINT FK_Employee_DepartmentID FOREIGN KEY (Department_ID) REFERENCES Department (Id), CONSTRAINT FK_Employee_JobPositionID FOREIGN KEY (JobPosition_ID) REFERENCES JobPosition (Id))";
                 using (MySqlCommand cmd = new MySqlCommand(createEmployeeTable, conn))
                 {
                     cmd.ExecuteNonQuery();
                     Console.WriteLine("Employee Tábla sikeresen létrehozva!");
                 }
 
-                string insertEmployeeTable = "INSERT INTO Employee (GrossWage, NetWage) VALUES (@GrossWage, @NetWage)";
+                string insertEmployeeTable = "INSERT INTO Employee (FirstName, LastName, GrossWage, NetWage, JobPosition_ID, Department_ID) VALUES (@FirstName, @LastName, @GrossWage, @NetWage, @JobPosition_ID, @Department_ID)";
+                string SelectJobPosID = "SELECT Id FROM JobPosition WHERE JobName = @JobTitle";
+                string SelectDepID = "SELECT Id FROM Department WHERE Name = @Dp";
                 using (MySqlCommand cmd = new MySqlCommand(insertEmployeeTable, conn))
                 {
                     for (int i = 0; i <= adatok.Count - 1; i++)
                     {
                         cmd.Parameters.Clear();
+                        int jobtitleid;
+                        int depid;
+                        using (MySqlCommand cmd2 = new MySqlCommand(SelectJobPosID, conn))
+                        {
+                            cmd2.Parameters.AddWithValue("@JobTitle", adatok[i].JobTitle);
+                            jobtitleid = (int)cmd2.ExecuteScalar();
+                        }
+                        using (MySqlCommand cmd3 = new MySqlCommand(SelectDepID, conn))
+                        {
+                            cmd3.Parameters.AddWithValue("@Dp", adatok[i].Department);
+                            depid = (int)cmd3.ExecuteScalar();
+                        }
+                        cmd.Parameters.AddWithValue("@FirstName", adatok[i].FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", adatok[i].LastName);
                         cmd.Parameters.AddWithValue("@GrossWage", adatok[i].GrossWage);
                         cmd.Parameters.AddWithValue("@NetWage", adatok[i].NetWage);
+                        cmd.Parameters.AddWithValue("@JobPosition_ID", jobtitleid);
+                        cmd.Parameters.AddWithValue("@Department_ID", depid);
                         cmd.ExecuteNonQuery();
                     }
                     Console.WriteLine("Employee tábla sikeresen feltöltve");
                 }
 
 
-                // History Tabel - Insert
-                string createHistoryTable = "CREATE TABLE IF NOT EXISTS History (People_ID INT, BeginDate DATE, EndDate DATE, CONSTRAINT FK_Hystory_PeopleID FOREIGN KEY (People_ID) REFERENCES People (Id))";
+                // History Table - Insert
+                string createHistoryTable = "CREATE TABLE IF NOT EXISTS History (BeginDate DATE, EndDate DATE, EmployeeID INT, CONSTRAINT FK_History_EmployeeID FOREIGN KEY (EmployeeID) REFERENCES Employee (Id))";
                 using (MySqlCommand cmd = new MySqlCommand(createHistoryTable, conn))
                 {
                     cmd.ExecuteNonQuery();
                     Console.WriteLine("History Tábla sikeresen létrehozva!");
                 }
 
-                string insertHistoryTable = "INSERT INTO History (BeginDate, EndDate) VALUES (@BeginDate, @EndDate)";
+                string insertHistoryTable = "INSERT INTO History (BeginDate, EndDate, EmployeeID) VALUES (@BeginDate, @EndDate, @EmployeeID)";
+                string SelectEmployeeID = "SELECT Employee.Id FROM Employee JOIN JobPosition ON JobPosition_ID = JobPosition.Id WHERE FirstName = @FN AND LastName = @LN AND GrossWage = @GW AND JobPosition.JobName = @JN";
                 using (MySqlCommand cmd = new MySqlCommand(insertHistoryTable, conn))
                 {
                     for (int i = 0; i <= adatok.Count - 1; i++)
                     {
                         cmd.Parameters.Clear();
+                        int EmpID;
+                        using (MySqlCommand cmd2 = new MySqlCommand(SelectEmployeeID, conn))
+                        {
+                            cmd2.Parameters.AddWithValue("@FN", adatok[i].FirstName);
+                            cmd2.Parameters.AddWithValue("@LN", adatok[i].LastName);
+                            cmd2.Parameters.AddWithValue("@GW", adatok[i].GrossWage);
+                            cmd2.Parameters.AddWithValue("@JN", adatok[i].JobTitle);
+                            EmpID = (int)cmd2.ExecuteScalar();
+                        }
                         cmd.Parameters.AddWithValue("@BeginDate", adatok[i].BeginDate);
                         cmd.Parameters.AddWithValue("@EndDate", adatok[i].EndDate);
+                        cmd.Parameters.AddWithValue("@EmployeeID", EmpID);
                         cmd.ExecuteNonQuery();
                     }
                     Console.WriteLine("History tábla sikeresen feltöltve");
                 }
-
-
-
-
-                //People (Firtname, Lastname, id), Department(id, departmentname), JobPosition(name, id), employee(grosswage, netwage, jobpos id, department id, people id, id), hisytory(people id, begin, end) 
             }
         }
     }
